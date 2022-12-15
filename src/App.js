6a73d4010410117135815e963b8cc12dc6369670
Prinyada <prinyadapinn@gmail.com>
2022-12-15 23:30:29 +0700

    add component button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Button from './components/button'
+import { BUTTON_TYPES } from './data/button';
 
-function App() {
+const App = () =>{
   return (
     <div className="App">
-      <p>Hello</p>
+      <div className="content">
+        <h1>Online Bulletin Board</h1>
+        <h5>for KMUTNB Community</h5>
+      </div>
+      <div className="button-wrapper">
+        <Button type={BUTTON_TYPES.PRIMARY} buttonText="ประกาศของหาย"/>
+        <Button type={BUTTON_TYPES.SECONDARY} buttonText="ประกาศของมือสอง"/>
+      </div>
     </div>
   );
 }

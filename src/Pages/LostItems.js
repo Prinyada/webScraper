8341bcd417ca7d7fd4160fc6da15d63308182e88
Prinyada <prinyadapinn@gmail.com>
2023-01-19 22:21:19 +0700

    ดึงข้อมูลได้แล้ว

--- src/Pages/LostItems.js
+++ src/Pages/LostItems.js
@@ -1,22 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../realtimeData/firebase-config";
+import StartFirebase from '../realtimeData/firebase-config';
+import React from 'react';
+import {ref, onValue} from 'firebase/database';
+import {Table} from 'react-bootstrap';
 
-function LostItems(){
-  const [dataLost, setDataLost] = useState();
-  const dataCollectionRef = collection(db, "kmutnbcommunity");
+const db = StartFirebase();
 
-  useEffect(() => {
-    const getData = async () => {
-      const data = await getDocs(dataCollectionRef);
-      console.log(data);
-    };
+export default class LostItems extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            data: []
+        }
+    }
 
-    getData();
-  }, []);
-  return (
-    <div>LostItems</div>
-  )
+    componentDidMount(){
+        const dbRef = ref(db,'scraper');
+
+        // console.log("this dbRef",dbRef);
+
+        onValue(dbRef, (snapshot)=>{
+            // console.log("this snapshot->",snapshot);
+            let records = [];
+            snapshot.forEach(childSnapshot=>{
+                // console.log(childSnapshot);
+                let keyName = childSnapshot.key;
+                console.log('====================================');
+                console.log("this keyName -> ",keyName);
+                console.log('====================================');
+                let dt = childSnapshot.val();
+                records.push({"key": keyName, "dt": dt})
+            });
+            console.log('====================================');
+            console.log("this records -> ",records);
+            console.log('====================================');
+            this.setState({data: records});
+            console.log('====================================');
+            console.log("this data -> ",this.state.data);
+            console.log('====================================');
+        });
+    }
+
+    render(){
+        return(
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Year</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                        <tr>
+                            {/* <td>{this.}</td> */}
+                        </tr>
+                </tbody>
+            </Table>
+        )
+    }
 }
-
-export default LostItems;

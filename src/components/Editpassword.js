01dfe1bc31986990a3b320b3739c05dfaedd090a
Prinyada <prinyadapinn@gmail.com>
2023-02-23 16:37:59 +0700

    update input box

--- src/components/Editpassword.js
+++ src/components/Editpassword.js
@@ -13,7 +13,6 @@ function Editpassword() {
 
   const [error, setError] = useState("");
 
-
   function editToDb() {
     let newP = parseInt(newPass);
     if (oldPass === passdb) {
@@ -42,27 +41,46 @@ function Editpassword() {
 
   return (
     <div className="editpassword-container">
-      <input
-        className="editpasswordInput"
+      <Input
+        style={{
+          width: 300,
+          fontSize: 16,
+          fontFamily: "Prompt",
+          marginTop: 10,
+        }}
         type="password"
         placeholder="old password"
-        onChange={ e => setOldPass(e.target.value)}
+        onChange={(e) => setOldPass(e.target.value)}
       />
-      { error === "oldpassER" && <p className="text-error">old password error</p>}
-      <input
-        className="editpasswordInput"
+      {error === "oldpassER" && (
+        <p className="text-error">old password error</p>
+      )}
+      <Input
+        style={{
+          width: 300,
+          fontSize: 16,
+          fontFamily: "Prompt",
+          marginTop: 10,
+        }}
         type="password"
         placeholder="new password"
-        onChange={ e => setNewPass(e.target.value)}
+        onChange={(e) => setNewPass(e.target.value)}
       />
-      <input
-        className="editpasswordInput"
+      <Input
+        style={{
+          width: 300,
+          fontSize: 16,
+          fontFamily: "Prompt",
+          marginTop: 10,
+        }}
         type="password"
         placeholder="confirm password"
-        onChange={ e => setconfirmNewPass(e.target.value)}
+        onChange={(e) => setconfirmNewPass(e.target.value)}
       />
-      { error === "newpassER" && <p className="text-error">new password not same</p>}
-      <div className="editconfirm-btn" onClick={editToDb}>Confirm</div>
+      {error === "newpassER" && (
+        <p className="text-error">new password not same</p>
+      )}
+      <Button style={{ marginTop: 10 }} type="primary" onClick={editToDb}>ยืนยัน</Button>
     </div>
   );
 }

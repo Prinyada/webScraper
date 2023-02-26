051f2bbd9ee370a011908c0c3b6434bfe8fe8c50
Prinyada <prinyadapinn@gmail.com>
2023-02-27 03:36:08 +0700

    update LostItems

--- src/Pages/LostItems.js
+++ src/Pages/LostItems.js
@@ -6,15 +6,24 @@ import './LostItems.css';
 function LostItems(props) {
     let data = props.dataLost;
 
-    // console.log("this data -> ",data);
+    // console.log("this day -> ",data[0].day);
+    // console.log("this month -> ",data[0].month);
+    // console.log("this detailPost -> ",data[0].detailPost);
+    // console.log("this year -> ",data[0].year);
     
     return(
         <div className="lost-container">
             <div className="lost-header">
                 <p className="header-1">ของหาย</p>
             </div>
-            <div>
-                ข้อความ
+            <div className="lost-content">
+                { data.map((lost,index) => {
+                    return (
+                        <div className="lost-1">
+                            
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
